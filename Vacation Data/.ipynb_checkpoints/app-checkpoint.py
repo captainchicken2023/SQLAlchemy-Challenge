0a7304710f3b89381ac436fc82d15e5265003dac
captainchicken2023 <captainchicken2023@gmail.com>
2023-08-20 15:27:22 -0500

    HW_update

--- Vacation Data/.ipynb_checkpoints/app-checkpoint.py
+++ Vacation Data/.ipynb_checkpoints/app-checkpoint.py
@@ -1,13 +1,21 @@
 # Import the dependencies.
-from flask import Flask, jsonify
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
 
