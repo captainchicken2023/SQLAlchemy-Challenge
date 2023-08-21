c09d822d83e50c6927aa389ffb6f017bd7103a0d
captainchicken2023 <captainchicken2023@gmail.com>
2023-08-21 15:55:37 -0500

    HW_update

--- Vacation Data/app.py
+++ Vacation Data/app.py
@@ -41,14 +41,66 @@ def home():
     return "Welcome"
 
 
+@app.route("/api/v1.0/precipitation")
+def Precipitation():
+
+# Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
+
+# Return the JSON representation of your dictionary.
+
+
+@app.route("/api/v1.0/stations")
+def Stations():
+
+# Return a JSON list of stations from the dataset.
+
+@app.route("/api/v1.0/tobs")
+def TOBS():
+
+# Query the dates and temperature observations of the most-active station for the previous year of data.
+
+# Return a JSON list of temperature observations for the previous year.
+
+
+@app.route("/api/v1.0/<start>")
+def Starting_Temps():
+
+# Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+
+# For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
+
+  
+
+@app.route("/api/v1.0/<start>/<end>")
+def Starting_Ending_Temps():
+
+# For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive.
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+# Hints
+# Join the station and measurement tables for some of the queries.
+
+# Use the Flask jsonify function to convert your API data to a valid JSON response object.
+
+
+
 #A start route that:
 
 #Accepts the start date as a parameter from the URL (2 points)
 
 #Returns the min, max, and average temperatures calculated from the given start date to the end of the dataset (4 points)
-
-
-
 
 
 
